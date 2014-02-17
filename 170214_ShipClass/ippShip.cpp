5479187ba781af2c098f62132e2a9a996e20315f
lianghaoz <kholianghao@gmail.com>
2014-02-17 20:43:53 +0800

    ShipClass update
    Visual studios prompt me to save my ship.cpp when trying to close vs.
    This happened just after I uploaded. Re-uploaded to be safe.

--- 170214_ShipClass/ippShip.cpp
+++ 170214_ShipClass/ippShip.cpp
@@ -58,26 +58,6 @@ void ippShip::Update()
 
 void ippShip::Render ()
 {
-	/*Vec3D dimensions(20.0f, 20.0f, 0);
-		
-	glEnable(GL_BLEND);
-	glBlendFunc (GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
-	glEnable(GL_TEXTURE_2D);
-	
-	glPushMatrix();
-		glTranslatef(position.x, position.y, position.z);
-		glColor4f( 1.0f, 1.0f, 1.0f, 1.0f );
-		glBegin(GL_QUADS);
-			glTexCoord2f( 0, 1 ); glVertex2f( 0, 0);
-			glTexCoord2f( 0, 0 ); glVertex2f( 0, dimensions.y);
-			glTexCoord2f( 1, 0 ); glVertex2f( dimensions.x, dimensions.y );
-			glTexCoord2f( 1, 1 ); glVertex2f( dimensions.x, 0);
-		glEnd();
-	glPopMatrix();
-
-	glDisable(GL_BLEND);
-	glDisable(GL_TEXTURE_2D);*/
-
 	// Rendering the ship
 	ship->DrawQuads ();
 
@@ -104,6 +84,8 @@ void ippShip::Movement ( int moveCode )
 
 void ippShip::FireBullet ( void )
 {
+	// Shoot out from the center of the ship
+
 	Vec3D tempPosition = position;
 
 	tempPosition.x += ( ship->GetLength () / 2 - 5.0f );
